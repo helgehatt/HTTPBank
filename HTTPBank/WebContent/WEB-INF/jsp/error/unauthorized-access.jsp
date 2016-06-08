5ef9cc6ea3eb73dfc5b7bd4b0af743345ae29510
helgehatt <helge.hatteland@gmail.com>
2016-06-08 15:11:07 +0200

    Access fix

---
+++ HTTPBank/WebContent/WEB-INF/jsp/error/unauthorized-access.jsp
@@ -0,0 +1,21 @@
+<%@ include file="../head.jsp" %>
+
+<style>
+
+#unauthorized-access {
+	text-align: center;
+	max-width: 800px;
+}
+
+</style>
+
+<%@ include file="navbar.jsp" %>
+
+<div id="unauthorized-access" class="container">
+	  <h2>Unauthorized access</h2>
+	  <br>
+	  <h4>Enter correct login information to access requested page.</h4>
+</div>
+
+</body>
+</html>
--- HTTPBank/src/ibm/controller/LoginController.java
+++ HTTPBank/src/ibm/controller/LoginController.java
@@ -2,34 +2,41 @@ package ibm.controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import ibm.db.DB;
 
 @WebServlet("/checkLogin")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private static final ArrayList<String> CURRENCIES = DB.getCurrencies();
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         
-        // TODO: DB Authentication: Set login status.
-		request.getSession().setAttribute("currencies", DB.getCurrencies());
+        HttpSession session = request.getSession();
+
+		session.setAttribute("currencies", CURRENCIES);
         
         try {
         	int userID = DB.checkLogin2(username, password);
 			if (userID > 0) {
-				request.getSession().setAttribute("user", DB.getUser(userID));
+				session.setAttribute("admin", false);
+				session.setAttribute("user", DB.getUser(userID));
 				response.sendRedirect("user/accounts");
 				
-			} else if (username.equals("admin") && password.equals("")) {			
+			} else if (username.equals("admin") && password.equals("")) {	
+				session.setAttribute("admin", true);
 				response.sendRedirect("admin/users");
 				
 			} else {
